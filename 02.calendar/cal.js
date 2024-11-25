8ca3ee0a57cea442bf9503dc8c987d73ca4bb000
SuzukiShuntarou <da8801593@gmail.com>
2024-11-25 22:26:31 +0900

    Change to Iterating through Date object

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -24,23 +24,27 @@ const generateCalendarDates = (options) => {
 };
 
 const showCalendar = (year, month, firstDate, lastDate) => {
-  const SATURDAY = 6;
   console.log(`${" ".repeat(6)}${month}月 ${year}`);
   console.log("日 月 火 水 木 金 土");
   process.stdout.write("   ".repeat(firstDate.getDay()));
 
-  Array.from(Array(lastDate.getDate()).keys(), (x) => x + 1).forEach((date) => {
-    const formattedDate = date.toString().padStart(2, " ");
+  const SATURDAY = 6;
+  for (
+    const currentDate = new Date(firstDate);
+    currentDate <= lastDate;
+    currentDate.setDate(currentDate.getDate() + 1)
+  ) {
+    const formattedDate = currentDate.getDate().toString().padStart(2, " ");
 
-    if (new Date(year, month - 1, date).getDay() != SATURDAY) {
+    if (currentDate.getDay() != SATURDAY) {
       process.stdout.write(formattedDate);
-      if (date < lastDate.getDate()) {
+      if (currentDate < lastDate) {
         process.stdout.write(" ");
       }
     } else {
       console.log(formattedDate);
     }
-  });
+  }
   console.log();
 };
 
