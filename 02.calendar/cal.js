2b5ae3997e7d9771a094a824b50f10db75271257
SuzukiShuntarou <da8801593@gmail.com>
2024-11-28 00:33:13 +0900

    Change to only generate the year and month

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -9,21 +9,22 @@ const main = () => {
     year: args.y,
     month: args.m,
   };
-  const [year, month, firstDate, lastDate] = generateCalendarDates(options);
-  showCalendar(year, month, firstDate, lastDate);
+  const [year, month] = generateCalendarYearMonth(options);
+  showCalendar(year, month);
 };
 
-const generateCalendarDates = (options) => {
+const generateCalendarYearMonth = (options) => {
   const today = new Date();
   const year = options.year ?? today.getFullYear();
   const month = options.month ?? today.getMonth() + 1;
+
+  return [year, month];
+};
+
+const showCalendar = (year, month) => {
   const firstDate = new Date(year, month - 1, 1);
   const lastDate = new Date(year, month, 0);
 
-  return [year, month, firstDate, lastDate];
-};
-
-const showCalendar = (year, month, firstDate, lastDate) => {
   console.log(`${" ".repeat(6)}${month}月 ${year}`);
   console.log("日 月 火 水 木 金 土");
   process.stdout.write("   ".repeat(firstDate.getDay()));
