1a6242d76dcfbc9ce619ee787fbb2cfb2ec28a38
SuzukiShuntarou <da8801593@gmail.com>
2024-12-15 16:15:56 +0900

    Use node-sqlite3 in async-await style

---
+++ 03.asynchronous/async/async.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+import timers from "timers/promises";
+
+import {
+  runDatabasePromise,
+  getDatabasePromise,
+  allDatabasePromise,
+  closeDatabasePromise,
+} from "../promise/promisification-functions.js";
+
+const createBooksTableWithoutError = async () => {
+  await runDatabasePromise(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  const result = await runDatabasePromise(
+    "INSERT INTO books (title) VALUES (?)",
+    "JavaScript Primer",
+  );
+  const insertedId = result.lastID;
+  console.log(insertedId);
+  const record = await getDatabasePromise(
+    "SELECT title FROM books WHERE id = ?",
+    insertedId,
+  );
+  console.log(record);
+  await runDatabasePromise("DROP TABLE books");
+};
+createBooksTableWithoutError();
+
+await timers.setTimeout(100);
+
+const handleSqliteError = (error) => {
+  if (error instanceof Error && error.code?.startsWith("SQLITE_")) {
+    console.log(error.message);
+  } else {
+    throw error;
+  }
+};
+
+const createBooksTableWithError = async () => {
+  await runDatabasePromise(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  try {
+    await runDatabasePromise(
+      "INSERT INTO books (titlr) VALUES (?)",
+      "JavaScript Primer",
+    );
+  } catch (error) {
+    handleSqliteError(error);
+  }
+  try {
+    await allDatabasePromise("SELECT * FROM book");
+  } catch (error) {
+    handleSqliteError(error);
+  }
+  try {
+    await runDatabasePromise("DROP TABLE books");
+  } finally {
+    await closeDatabasePromise();
+  }
+};
+createBooksTableWithError();
