44e7d62369ba27e8e857875e1bdd5126db5f3a58
SuzukiShuntarou <da8801593@gmail.com>
2024-12-18 21:08:55 +0900

    Split callback without error into function definitions and main

--- 03.asynchronous/callback/callback.js
+++ 03.asynchronous/callback/callback.js
@@ -5,29 +5,39 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const createBooksTableWithoutError = () => {
+const createBooksTable = (callback) => {
   db.run(
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
     () => {
-      db.run(
-        "INSERT INTO books (title) VALUES (?)",
-        "JavaScript Primer",
-        function () {
-          console.log(this.lastID);
-          db.get(
-            "SELECT title FROM books WHERE id = ?",
-            this.lastID,
-            (_, record) => {
-              console.log(record);
-              db.run("DROP TABLE books");
-            },
-          );
-        },
-      );
+      callback();
     },
   );
 };
-createBooksTableWithoutError();
+
+const insertBookTitle = (title, callback) => {
+  db.run("INSERT INTO books (title) VALUES (?)", title, function () {
+    callback(this.lastID);
+  });
+};
+
+const findBookColumnById = (id, callback) => {
+  db.get("SELECT title FROM books WHERE id = ?", id, (_, record) => {
+    callback(record);
+  });
+};
+
+const mainWithoutError = () => {
+  createBooksTable(() => {
+    insertBookTitle("JavaScript Primer", (lastId) => {
+      console.log(lastId);
+      findBookColumnById(lastId, (record) => {
+        console.log(record);
+        db.run("DROP TABLE books");
+      });
+    });
+  });
+};
+mainWithoutError();
 
 await timers.setTimeout(100);
 
